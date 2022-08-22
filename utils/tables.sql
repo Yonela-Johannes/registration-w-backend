d6bb3c3b1da6e516c5d8bb179ebe60e62363f941
Yonela-Johannes <johannesyonela@gmail.com>
2022-08-22 10:57:05 +0200

    insert towns into db tests

--- utils/tables.sql
+++ utils/tables.sql
@@ -10,3 +10,14 @@ CREATE TABLE regnumbers (
     town_id INT,
     FOREIGN KEY (town_id) REFERENCES towns(ID) ON DELETE CASCADE
 );
+
+INSERT INTO towns (town, regcode) VALUES 
+('Cape Town', 'ca'),
+('Stellenbosch', 'ca'),
+('Bellville', 'cy'),
+('Kuils River', 'cf'),
+('Wellington', 'cn'),
+('Malmesbury', 'ck'),
+('Worcester', 'cw'),
+('Ceres', 'ct'),
+('George', 'caw')
